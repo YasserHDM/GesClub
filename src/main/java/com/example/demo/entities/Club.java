70725e1a278528925eace724a6a0a9a4ad6606ee
Yasser Haddam <61160399+YasserHDM@users.noreply.github.com>
2021-10-30 12:07:45 +0100

    Add Readme

--- src/main/java/com/example/demo/entities/Club.java
+++ src/main/java/com/example/demo/entities/Club.java
@@ -1,4 +1,13 @@
 package com.example.demo.entities;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
 public class Club {
 }
