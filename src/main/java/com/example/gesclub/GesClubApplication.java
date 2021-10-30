1130c2956e2da9a5c2cbd04823d6aab4afe49c98
Yasser Haddam <61160399+YasserHDM@users.noreply.github.com>
2021-10-30 09:16:03 +0100

    Initial commit

---
+++ src/main/java/com/example/gesclub/GesClubApplication.java
@@ -0,0 +1,13 @@
+package com.example.gesclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GesClubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GesClubApplication.class, args);
+    }
+
+}
